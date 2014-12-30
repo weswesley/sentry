d62726ad2ae5de89e65a45520baba73fb2bba2a0
Wes Almeida <wes@scientiamobile.com>
2014-12-30 17:27:02 -0500

    initial commit again

---
+++ src/wesleyalmeida/Sentry/SentryServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Wesleyalmeida\Sentry;
+
+use Illuminate\Support\ServiceProvider;
+
+class SentryServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('wesleyalmeida/sentry');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
