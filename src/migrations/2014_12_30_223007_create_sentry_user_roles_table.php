dce5d29f7a53a714b449e03df395d7b987cdf334
Wes Almeida <wes@scientiamobile.com>
2014-12-30 17:51:02 -0500

    tables

---
+++ src/migrations/2014_12_30_223007_create_sentry_user_roles_table.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Author: Wesley
+ * Date: 30-Dec-14
+ * Time: 12:00
+ */
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSentryUserRolesTable extends Migration {
+
+	public function up() {
+		Schema::create('sentry_user_roles', function($table) {
+			$table->primary(['user_id', 'role']);
+		});
+	}
+
+	public function down() {
+		Schema::drop('sentry_user_roles');
+	}
+
+}
