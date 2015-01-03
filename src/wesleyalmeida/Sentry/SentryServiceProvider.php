39c50837c84cfc46e887b17ef2f8cc61814db5dc
Wes Almeida <wes@scientiamobile.com>
2015-01-02 19:58:35 -0500

    service provider update

--- src/wesleyalmeida/Sentry/SentryServiceProvider.php
+++ src/wesleyalmeida/Sentry/SentryServiceProvider.php
@@ -34,7 +34,7 @@ class SentryServiceProvider extends ServiceProvider {
 
 		App::bindShared('sentry', function($app) {
 
-			$defaults = Config::get('sentry::defaults');
+			$defaults = Config::get('sentry::sentry.defaults');
 
 			$sentry = new Sentry;
 			$sentry->setDefaults($defaults);
